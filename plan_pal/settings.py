910a6a3ee20b5ec9ba58d4c312130a20c84f119a
KristiyanYanakiev <yankris_92@yahoo.com>
2026-04-07 03:24:01 +0300

    chore: fixed allowed hosts

--- plan_pal/settings.py
+++ plan_pal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 
-ALLOWED_HOSTS = [host for host in os.getenv('ALLOWED_HOSTS').split(',') if host]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
