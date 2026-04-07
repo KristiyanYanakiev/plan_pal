b8cafc95b2c01d3ded0e79ba2fd6f8f51819f828
KristiyanYanakiev <yankris_92@yahoo.com>
2026-04-07 05:00:44 +0300

    feat: fix media files in templates

--- plan_pal/settings.py
+++ plan_pal/settings.py
@@ -151,7 +151,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'accounts:login'
--- plan_pal/urls.py
+++ plan_pal/urls.py
@@ -36,8 +36,12 @@ urlpatterns = [
 def custom_404(request, exception):
     return render(request, 'errors/404.html', status=404)
 
+def custom_500(request):
+    return render(request, 'errors/500.html', status=500)
 
 handler404 = custom_404
+handler500 = custom_500
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
